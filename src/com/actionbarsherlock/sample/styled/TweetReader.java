69459a3cdc75240deefe5c0575cd3136f261fc77
Kashif Smith <smithkashif@gmail.com>
2013-02-22 14:30:53 -0500

    only took 3 hours!

--- src/com/actionbarsherlock/sample/styled/TweetReader.java
+++ src/com/actionbarsherlock/sample/styled/TweetReader.java
@@ -34,7 +34,7 @@ public class TweetReader {
 
 		@Override
 		protected Void doInBackground(Object... params) {
-			//working automated push 1
+			//only took 3 hours!
 			Twitter twitter = (Twitter)params[0];
 			//			List<twitter4j.Status> statuses = retrieveSpecificUsersTweets(twitter);
 			//			convertTimelineToJson(statuses);
