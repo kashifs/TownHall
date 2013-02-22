27ba5eb1a6411bcf92a45991677b5f72fa4df5a8
Kashif Smith <smithkashif@gmail.com>
2013-02-22 14:02:31 -0500

    asga

--- src/com/actionbarsherlock/sample/styled/TweetReader.java
+++ src/com/actionbarsherlock/sample/styled/TweetReader.java
@@ -34,7 +34,7 @@ public class TweetReader {
 
 		@Override
 		protected Void doInBackground(Object... params) {
-			//thirdautomatedpush
+			//fifthautomatedpush
 			Twitter twitter = (Twitter)params[0];
 			//			List<twitter4j.Status> statuses = retrieveSpecificUsersTweets(twitter);
 			//			convertTimelineToJson(statuses);
